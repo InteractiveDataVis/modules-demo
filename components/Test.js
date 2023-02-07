d945eedbae594020e26b2b2e3926190e7197e8c6
ellie <ellie.frymire@gmail.com>
2023-02-07 11:00:35 -0800

    blank slate

--- components/Test.js
+++ components/Test.js
@@ -1,20 +1,13 @@
 class TestClassComponent {
 
   // class fields
-  something = "something"
+
 
   // constructor
-  constructor(initialProps) {
-    console.log(initialProps)
-    this.method()
-    this.method2()
-  }
+
 
   // class methods
-  method() {
-    console.log("method1")
-    console.log(this)
-  }
+
 
 }
 
